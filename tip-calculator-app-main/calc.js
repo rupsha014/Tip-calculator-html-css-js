a3343b1c4710c4749ba1af2018d18042a886c923
rupsha014 <rupshasarkar14@gmail.com>
2022-09-26 23:41:45 -0700

    commit

--- tip-calculator-app-main/calc.js
+++ tip-calculator-app-main/calc.js
@@ -1,1 +1,11 @@
+ const calculating_app=document.getElementsByClassName('calculator-app');
+ const display=document.getElementsByClassName('dis');
+ const amt=document.getElementsByClassName('amt-display');
+ const calc=document.getElementsByClassName('calculation');
+ const tot=document.getElementsByClassName('total');
+ const Tips=document.getElementsByClassName('Tip');
+ const peop=document.getElementsByClassName('people');
+ const sum=document.getElementsByClassName('sumation');
+ const tip-am=document.getElementsByClassName('tip-amount');
+ const tot-am=document.getElementsByClassName('total-amount');
  
